533df88e7f8cd5b64c4488c227893ee9fdbeb346
Jeremy Luzzi <jeremyluzzidev@gmail.com>
2023-05-17 20:46:53 -0600

    feat: handle key press events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import NavigationBar from "./pages/NavigationBar";
 
@@ -9,10 +9,20 @@ export const App = () => {
   const [images, setImages] = useState([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const handleNextImage = () =>
-    setCurrentImageIndex(Math.min(currentImageIndex + 1, images.length - 1));
-  const handlePreviousImage = () =>
-    setCurrentImageIndex(Math.max(currentImageIndex - 1, 0));
+  const handleNextImage = useCallback(
+    () =>
+      setCurrentImageIndex((currentImageIndex) =>
+        Math.min(currentImageIndex + 1, images.length - 1)
+      ),
+    [images.length]
+  );
+  const handlePreviousImage = useCallback(
+    () =>
+      setCurrentImageIndex((currentImageIndex) =>
+        Math.max(currentImageIndex - 1, 0)
+      ),
+    []
+  );
 
   //TODO: API functions (more to be added) should be in their own file!
   const getEvents = () => {
@@ -31,6 +41,22 @@ export const App = () => {
   useEffect(() => {
     getEvents();
   }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowLeft") {
+        handlePreviousImage();
+      } else if (event.key === "ArrowRight") {
+        handleNextImage();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNextImage, handlePreviousImage, images.length]);
 
   return (
     //TODO: This code should be factored out into multiple files
