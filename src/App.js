1d6abd74db2f9c53576523bcbf7b2aa132f55846
Jeremy <Jluzz403@gmail.com>
2023-05-17 20:43:00 -0600

    feat/right-left-arrow-buttons (#2)
    * feat: create image handlers
    
    * chore: bound image handlers
    
    * chore: update import order
    
    * chore: update wording
    
    * chore: add gap
    
    * chore: update imports
    
    * feat: handle key press events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import "./App.css";
-import logo from './logo.svg';
-
-import { useEffect, useState } from "react";
-import NavigationBar from "./pages/NavigationBar";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import NavigationBar from "./pages/NavigationBar";
 
-function App() {
+import "./App.css";
+
+export const App = () => {
   const baseURL = "http://localhost:7071";
   const [images, setImages] = useState([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextImage = useCallback(
+    () =>
+      setCurrentImageIndex((currentImageIndex) =>
+        Math.min(currentImageIndex + 1, images.length - 1)
+      ),
+    [images.length]
+  );
+  const handlePreviousImage = useCallback(
+    () =>
+      setCurrentImageIndex((currentImageIndex) =>
+        Math.max(currentImageIndex - 1, 0)
+      ),
+    []
+  );
 
   //TODO: API functions (more to be added) should be in their own file!
   const getEvents = () => {
@@ -27,6 +41,22 @@ function App() {
   useEffect(() => {
     getEvents();
   }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowLeft") {
+        handlePreviousImage();
+      } else if (event.key === "ArrowRight") {
+        handleNextImage();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNextImage, handlePreviousImage, images.length]);
 
   return (
     //TODO: This code should be factored out into multiple files
@@ -47,12 +77,14 @@ function App() {
           justifyContent: "center",
           alignItems: "center",
           alignContent: "center",
+          gap: "24px",
           width: "85%",
           height: "100%",
         }}
       >
-        {/* TODO: This button does nothing!  */}
-        <button type="button">Previous Image</button>
+        <button type="button" onClick={handlePreviousImage}>
+          Previous Image
+        </button>
         <div>
           <div
             style={{
@@ -62,9 +94,11 @@ function App() {
             }}
           >
             <div> {images.length} total images </div>
-            <div> Index: {currentImageIndex} </div>
+            <div> Current Image: {currentImageIndex + 1} </div>
           </div>
-          {images.length > 0 && <img src={images[currentImageIndex].jpg} />}
+          {images.length > 0 && (
+            <img src={images[currentImageIndex].jpg} alt="current-scan" />
+          )}
           {images[currentImageIndex]?.createdOn && (
             <div> Scan Timestamp: {images[currentImageIndex].createdOn} </div>
           )}
@@ -72,11 +106,10 @@ function App() {
           <div> Image Metadata: INCOMPLETE </div>
           <div> Number of Detections: INCOMPLETE </div>
         </div>
-        {/* TODO: This button also does nothing  */}
-        <button type="button">Next Image</button>
+        <button type="button" onClick={handleNextImage}>
+          Next Image
+        </button>
       </div>
     </div>
   );
-}
-
-export default App;
+};
