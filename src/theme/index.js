0f2a6aaf04463d751ad119b1f729a6015d86267e
Jeremy Luzzi <jeremyluzzidev@gmail.com>
2023-05-21 15:26:08 -0600

    refactor: export router and themeing

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    brand: [
+      "#F6F2F1",
+      "#E7D8D4",
+      "#DCBFB6",
+      "#D7A697",
+      "#D88D75",
+      "#E0714F",
+      "#F05423",
+      "#D24F25",
+      "#AE4F31",
+      "#914D37",
+      "#7A493A",
+      "#68453A",
+      "#5A4038",
+    ],
+  },
+  primaryColor: "brand",
+};
