c1659f3f8ea2585eddb7bc09d13440b9a8888f63
Jeremy Luzzi <jeremyluzzidev@gmail.com>
2023-05-21 19:46:42 -0600

    chore: add key prop

--- src/components/EventViewer/Viewport/Viewport.js
+++ src/components/EventViewer/Viewport/Viewport.js
@@ -8,8 +8,8 @@ const Container = styled("div")({
 
 export const Viewport = ({ images, index }) => (
   <Container>
-    {images[index].detectionsList.map(({ roicoordsList }) => (
-      <DetectionBox coordinates={roicoordsList} />
+    {images[index].detectionsList.map(({ roicoordsList }, i) => (
+      <DetectionBox key={i} coordinates={roicoordsList} />
     ))}
     <img src={images[index].jpg} alt="current-scan" />
   </Container>
